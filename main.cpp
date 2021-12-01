dd873618f83923d51fa2d831b9e2f67db25a7b6c
Loddie02 <91880336+Loddie02@users.noreply.github.com>
2021-12-01 23:13:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@ int main() {
     char conversion;
     cin>>input;
 
-    result = convert(input);
-    cout << result<< endl;
+    result=convert(input);
+    cout<<result<<endl;
 
-    if (result == true) {
-        if (input >= 65 && input <= 90) {
-            conversion = input + 32;
+    if (result==true) {
+        if (input>=65 && input<=90) {
+            conversion= nput+32;
         }
-        if (input >= 97 && input <= 122) {
-            conversion = input - 32;
+        if (input>=97 && input<=122) {
+            conversion=input-32;
         }
         cout<<conversion<<endl;
     } else {
