d92d632b2b587e5cd8afbf5e993f817172924588
Loddie02 <91880336+Loddie02@users.noreply.github.com>
2021-12-01 23:29:55 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,17 +1,16 @@
 #include "lib.h"
 
 bool convert(char &c) {
-   char conversion;
    bool result;
 
     if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
         if (c >= 65 && c <= 90) {
-            conversion = c+32;
+            c = c+32;
 
             return  &c;
         }
         if (c >= 97 && c <= 122){
-            conversion = c-32;
+            c = c-32;
             return  &c;
 
         }
