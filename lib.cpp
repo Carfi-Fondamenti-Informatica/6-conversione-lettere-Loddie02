07bfe54b5c6a43fb8dd4b5d9cbfc17bac37902fb
Loddie02 <91880336+Loddie02@users.noreply.github.com>
2021-12-01 23:08:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+bool convert(char c) {
+   char conversion;
+
+    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
+        if (c >= 65 && c <= 90) {
+            conversion = c+32;
+        }
+        if (c >= 97 && c <= 122){
+            conversion = c-32;
+        }
+        return true;
+    } else {
+        return false;
+    }
+
+}
