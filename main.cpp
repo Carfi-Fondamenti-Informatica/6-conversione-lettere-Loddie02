8bbb0ba12d45490a60532c6429320946779d00bb
Loddie02 <91880336+Loddie02@users.noreply.github.com>
2021-12-01 23:09:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
+using namespace std;
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+
+int main() {
+    char input;
+    bool result;
+    char conversion;
+    cin>>input;
+
+    result = convert(input);
+    cout << result<< endl;
+
+    if (result == true) {
+        if (input >= 65 && input <= 90) {
+            conversion = input + 32;
+        }
+        if (input >= 97 && input <= 122) {
+            conversion = input - 32;
+        }
+        cout<<conversion<<endl;
+    } else {
+        cout<<"errore";
+    }
+    return 0;
 }
