8284eede7db32be5f8bce2b531378bcceda04e23
Loddie02 <91880336+Loddie02@users.noreply.github.com>
2021-12-01 23:11:28 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,14 +1,14 @@
 #include "lib.h"
 
-bool convert(char c) {
+bool convert(charc) {
    char conversion;
 
-    if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
-        if (c >= 65 && c <= 90) {
-            conversion = c+32;
+    if (c>=65 && c<=90 || c>=97 && c<=122) {
+        if (c>=65 && c<=90) {
+            conversion=c+32;
         }
-        if (c >= 97 && c <= 122){
-            conversion = c-32;
+        if (c>=97 && c<=122){
+            conversion=c-32;
         }
         return true;
     } else {
